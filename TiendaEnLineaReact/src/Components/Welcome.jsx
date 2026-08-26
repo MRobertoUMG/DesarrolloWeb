2a0558a3d58f4473c186cfc406ddbee4c625dfff
MRobertoUMG <mrompichy@miumg.edu.gt>
2026-08-26 10:17:20 -0600

    Welcome component

---
+++ TiendaEnLineaReact/src/Components/Welcome.jsx
@@ -0,0 +1,8 @@
+export default function Welcome() {
+  return (
+    <div className="welcome text-center">
+      <h1>Bienvenido a nuestra tienda</h1>
+      <p>Explora nuestros productos y encuentra lo que necesitas.</p>
+    </div>
+  )
+}
